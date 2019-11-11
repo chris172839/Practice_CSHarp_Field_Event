35576a9af49b4a007c52cc3182afccaf32e00804
chris172839 <57192859+chris172839@users.noreply.github.com>
2019-11-11 21:06:26 +0800

    1108HW

---
+++ UnityProject/Assets/Scripts/Chicken.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Chicken : MonoBehaviour
+{
+	[Header("移動速度"),Range(1f,100f)]
+	public float speed = 1f;
+	[Header("跳躍高度"),Range(1,2000)]
+	public int jump=10;
+	[Header("對話內容")]
+	public string talk="咕咕咕~~~";
+	[Header("是否取得雞蛋")]
+	public bool Egg;
+    void Start()
+    {
+        print("遊戲開始!!!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(talk);
+    }
+}
---
+++ UnityProject/Assets/chick.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class chick : MonoBehaviour
+{
+	[Range(1f,100f)]
+	public float speed = 1f;
+}
